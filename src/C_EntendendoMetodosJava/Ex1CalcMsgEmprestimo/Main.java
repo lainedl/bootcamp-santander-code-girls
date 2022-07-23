ad9535a23ec3474f345e970e52aaaaa7476df60f
Edilaine Dias Lima <layne.dl@gmail.com>
2022-07-23 19:19:04 -0300

    First commit - Métodos

---
+++ src/C_EntendendoMetodosJava/Ex1CalcMsgEmprestimo/Main.java
@@ -0,0 +1,28 @@
+package C_EntendendoMetodosJava.Ex1CalcMsgEmprestimo;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //Calculadora
+        System.out.println("Exercicio calculadora");
+        Calculadora.soma(3, 6);
+        Calculadora.subtracao(9, 1.8);
+        Calculadora.multiplicacao(7, 8);
+        Calculadora.divisao(5, 2.5);
+        //}
+
+        //Mensagem
+        System.out.println("Exercicio mensagem");
+        Mensagem.obterMensagem(6);
+        Mensagem.obterMensagem(15);
+        Mensagem.obterMensagem(22);
+        Mensagem.obterMensagem(72);
+
+        //Empréstimo
+        System.out.println("Exercicio empréstimo");
+        Emprestimo.calcular(1000, Emprestimo.getDuasParcelas());
+        Emprestimo.calcular(1000, Emprestimo.getTresParcelas());
+        Emprestimo.calcular(1000,5);
+    }
+}
